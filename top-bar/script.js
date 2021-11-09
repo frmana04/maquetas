154f9e8efaaa192a1a73a501c65eaeb5a77d1e2c
frmv <frmv@gft.com>
2021-11-09 22:35:28 +0100

    ajustes

--- top-bar/script.js
+++ top-bar/script.js
@@ -97,27 +97,27 @@ function createMenu() {
     menuObject.options.forEach((option, index) => {
         htmlText += `
        
-        <div class="header__option" id="${option.id}" onclick="${device=='DK'?'setAllInactive()':''};setActive('opt${index+1}')" data-active="false">
-        ${device=='MB'?'<div class="header__arrow-container-left"><img class="header__arrow-img" src="assets/images/arrow_left.png"></div>':""}
-        ${option.ico?`<img class="header__language-img" src="assets/images/${option.nameIco}">`:""}
+        <div class="bar__option" id="${option.id}" onclick="${device=='DK'?'setAllInactive()':''};setActive('opt${index+1}')" data-active="false">
+        ${device=='MB'?'<div class="bar__arrow-container-left"><img class="bar__arrow-img" src="assets/images/arrow_left.png"></div>':""}
+        ${option.ico?`<img class="bar__language-img" src="assets/images/${option.nameIco}">`:""}
       
-        <span class="header__text">${option.name}</span>${device=='MB'?'<div class="header__arrow-container-right"><img class="header__arrow-img" src="assets/images/arrow_right.png"></div>':""}
-        <div class="header__submenu submenu--inactive">
+        <span class="bar__text">${option.name}</span>${device=='MB'?'<div class="bar__arrow-container-right"><img class="bar__arrow-img" src="assets/images/arrow_right.png"></div>':""}
+        <div class="bar__submenu submenu--inactive">
         ${option.subOptions.map(subOption=>{
-            return(`<a class="header__link" onclick="${option.id=="opt4"?'setLanguage('+"\'"+subOption.name+"\'"+')':''}" href="${subOption.url}"><div class="header__submenu-opt"><span class="header__submenu-text">${subOption.name}</span><div class="header__arrow-container-right"><img class="header__arrow-img" src="assets/images/arrow_right.png"></div></div></a>`)
+            return(`<a class="bar__link" onclick="${option.id=="opt4"?'setLanguage('+"\'"+subOption.name+"\'"+')':''}" href="${subOption.url}"><div class="bar__submenu-opt"><span class="bar__submenu-text">${subOption.name}</span><div class="bar__arrow-container-right"><img class="bar__arrow-img" src="assets/images/arrow_right.png"></div></div></a>`)
         }).join("")}    
         </div>
     
     </div>
     `
        if (device=="DK"){
-        document.querySelector('.header__options--DK').innerHTML = htmlText;
-        document.querySelector('.header__options--MB').style.display="none";
+        document.querySelector('.bar__options--DK').innerHTML = htmlText;
+        document.querySelector('.bar__options--MB').style.display="none";
 
        }
        else{
-        document.querySelector('.header__options--MB').innerHTML = htmlText;
-        document.querySelector('.header__options--DK').style.display="none";
+        document.querySelector('.bar__options--MB').innerHTML = htmlText;
+        document.querySelector('.bar__options--DK').style.display="none";
        }
      
     });
@@ -125,7 +125,7 @@ function createMenu() {
 
 function setLanguage(language){
 
-    document.querySelector('#opt4').querySelector('.header__text').innerHTML=language;
+    document.querySelector('#opt4').querySelector('.bar__text').innerHTML=language;
 
 }
 
@@ -133,19 +133,19 @@ function attachEvents() {
     if (device == "DK") {
         document.querySelector('#opt4').addEventListener('mouseenter', () => {
 
-            document.querySelector('.header__language-img').src = "./assets/images/language-black.png";
+            document.querySelector('.bar__language-img').src = "./assets/images/language-black.png";
         });
 
         document.querySelector('#opt4').addEventListener('mouseleave', () => {
 
             if (optActive != "opt4")
-                document.querySelector('.header__language-img').src = "./assets/images/language.png";
+                document.querySelector('.bar__language-img').src = "./assets/images/language.png";
         });
 
         document.querySelector('#opt4').addEventListener('mouseleave', () => {
 
             if (optActive != "opt4")
-                document.querySelector('.header__language-img').src = "./assets/images/language.png";
+                document.querySelector('.bar__language-img').src = "./assets/images/language.png";
         });
 
       
@@ -166,7 +166,7 @@ function attachEvents() {
 
         window.addEventListener('click', $ev => {
 
-            let arrayNodes = document.querySelectorAll('.header__option');
+            let arrayNodes = document.querySelectorAll('.bar__option');
             let contains = false;
             arrayNodes.forEach(node => {
                 contains ||= node.contains($ev.target);
@@ -178,7 +178,7 @@ function attachEvents() {
         });
     }
     else {
-    document.querySelectorAll('.header__link').forEach(el=>{
+    document.querySelectorAll('.bar__link').forEach(el=>{
         console.log(el)
         el.addEventListener('click',(ev)=>{
         ev.stopPropagation();
@@ -200,14 +200,14 @@ function setInactive(id) {
 
 
     document.querySelector(`#${id}`).dataset.active = false;
-    document.querySelector(`#${id}`).querySelector('.header__submenu').style.display = "none";
+    document.querySelector(`#${id}`).querySelector('.bar__submenu').style.display = "none";
     document.querySelector(`#${id}`).classList.remove('submenu--active');
     document.querySelector(`#${id}`).classList.add('submenu--inactive');
 
     if (device=="MB"){
 
-        document.querySelector(`#${id}`).querySelector('.header__arrow-container-left').style.display="none";
-        document.querySelector(`#${id}`).querySelector('.header__arrow-container-right').style.display="flex";
+        document.querySelector(`#${id}`).querySelector('.bar__arrow-container-left').style.display="none";
+        document.querySelector(`#${id}`).querySelector('.bar__arrow-container-right').style.display="flex";
         menuObject.options.forEach(option=>{
            
              document.querySelector(`#${option.id}`).style.display="flex";
@@ -217,7 +217,7 @@ function setInactive(id) {
     
 
     if (id == 'opt4')
-        document.querySelector('.header__language-img').src = "./assets/images/language.png";
+        document.querySelector('.bar__language-img').src = "./assets/images/language.png";
 
 
     optActive = (optActive == id || !optActive) ? null : optActive;
@@ -227,11 +227,11 @@ function setActive(id) {
 
         if(device=="DK"){
             document.querySelector(`#${id}`).dataset.active = true;
-            document.querySelector(`#${id}`).querySelector('.header__submenu').style.display = "block";
+            document.querySelector(`#${id}`).querySelector('.bar__submenu').style.display = "block";
             document.querySelector(`#${id}`).classList.remove('submenu--inactive');
             document.querySelector(`#${id}`).classList.add('submenu--active');        
             if (id == 'opt4')
-                document.querySelector('.header__language-img').src = "./assets/images/language-black.png";
+                document.querySelector('.bar__language-img').src = "./assets/images/language-black.png";
             optActive = id;
         }
       
@@ -245,9 +245,9 @@ function setActive(id) {
         else{
 
             document.querySelector(`#${id}`).dataset.active = true;
-            document.querySelector(`#${id}`).querySelector('.header__submenu').style.display = "block";
-            document.querySelector(`#${id}`).querySelector('.header__arrow-container-left').style.display="block";
-            document.querySelector(`#${id}`).querySelector('.header__arrow-container-right').style.display="none";
+            document.querySelector(`#${id}`).querySelector('.bar__submenu').style.display = "block";
+            document.querySelector(`#${id}`).querySelector('.bar__arrow-container-left').style.display="block";
+            document.querySelector(`#${id}`).querySelector('.bar__arrow-container-right').style.display="none";
             menuObject.options.forEach(option=>{
                 if (id !=option.id){
                  document.querySelector(`#${option.id}`).style.display="none";
@@ -263,13 +263,13 @@ function setActive(id) {
 
 function toggleMenuMB() {
     if (stateMenuMB == 'closed') {
-        document.querySelector('.header__burger').src = "./assets/images/close.png"
-        document.querySelector('.header__options').style.top="52px";
+        document.querySelector('.bar__burger').src = "./assets/images/close.png"
+        document.querySelector('.bar__options').style.top="52px";
         stateMenuMB = 'opened';
     } else if (stateMenuMB == 'opened') {
-        document.querySelector('.header__burger').src = "./assets/images/ico-menu-mobile.png"
+        document.querySelector('.bar__burger').src = "./assets/images/ico-menu-mobile.png"
         stateMenuMB = 'closed';
-        document.querySelector('.header__options').style.top="-192px";
+        document.querySelector('.bar__options').style.top="-192px";
 
         setAllInactive();
         menuObject.options.forEach(option=>{
