911581ad704c0b6a1dc9801ad7ae1ed84a3c7c1e
frmv <frmv@gft.com>
2021-11-16 19:13:14 +0100

    participantes

---
+++ participantes/scripts.js
@@ -0,0 +1,21 @@
+
+
+function toggleMostrar(id){
+
+    let node = document.querySelector(`#${id}`);
+    if (node.dataset.state=="open"){
+
+        node.querySelector('.equipo__pilotos').style.display="none";
+        node.querySelector('.equipo__mostrar-texto').innerHTML="Mostrar detalles del equipo";
+        node.querySelector('.equipo__mostrar-arrow').style.transform = "rotate(90deg)";
+        node.dataset.state="close";
+    }
+    else{
+        node.querySelector('.equipo__pilotos').style.display="grid";
+        node.querySelector('.equipo__mostrar-texto').innerHTML="Ocultar detalles del equipo";
+        node.querySelector('.equipo__mostrar-arrow').style.transform = "rotate(270deg)";
+        node.dataset.state="open";
+    }
+   
+
+}
