be57a520ba02003e62d74bed8bd075a58aaef0b1
frmv <frmv@gft.com>
2021-11-29 00:31:25 +0100

    filtro

--- noticias/script.js
+++ noticias/script.js
@@ -1,11 +1,31 @@
 let STEP_NEWS = 3;
 let rowsShow = 0;
-
-
+let filtered =false;
+let  expanded = false;
 function showNexts() {
+    let nodes,deviceDisplay;
+    
+    if (filtered && document.documentElement.clientWidth <= 500 ){
+        nodes = document.querySelector('#filtered-grids').querySelectorAll('.news__grid-MB__new');
+        deviceDisplay="flex";
+    }
+       
 
-    let deviceDisplay = document.documentElement.clientWidth <= 500 ? "flex" : "grid";
-    let nodes = deviceDisplay == "grid" ? document.querySelectorAll('.news__grid-DK') : document.querySelectorAll('.news__grid-MB__new');
+    else if (filtered && document.documentElement.clientWidth > 500 ){
+        nodes = document.querySelector('#filtered-grids').querySelectorAll('.news__grid-DK');
+        deviceDisplay="grid";
+    }
+    
+    else if (!filtered && document.documentElement.clientWidth > 500 ){
+        nodes = document.querySelector('#original-grids-DK').querySelectorAll('.news__grid-DK');
+        deviceDisplay="grid";
+    }
+
+    else if (!filtered && document.documentElement.clientWidth <= 500 ){
+        nodes = document.querySelector('#original-grids-MB').querySelectorAll('.news__grid-MB__new');
+        deviceDisplay="flex";
+    }
+
 
     for (let i = 0; i < rowsShow + STEP_NEWS; i++)
         if (nodes[i])
@@ -15,6 +35,10 @@ function showNexts() {
     if (rowsShow >= nodes.length) {
         document.querySelector('.button--news').style.opacity = "0.5";
         document.querySelector('.button--news').style.pointerEvents = "none";
+    }
+    else{
+        document.querySelector('.button--news').style.opacity = "1";
+        document.querySelector('.button--news').style.pointerEvents = "auto";
     }
 }
 
@@ -30,31 +54,30 @@ function generateRandomTags() {
 function searchNewsbyTag(tags=["et-1","et-2"]) {
   
     let news=[];
-    document.querySelector('#original-grids').querySelectorAll('.new-box__tag').forEach(node => {
+    document.querySelector('#original-grids-DK').querySelectorAll('.new-box__tag').forEach(node => {
        if(tags.indexOf( node.innerHTML)!=-1)
         news.push(node.parentNode.dataset.new);
     });
     news = [...new Set(news)];
-    console.log(news)
-
+    return news;
 }
 
 function getContent(id,box){
 
     
-    return id!=""?document.querySelector('#original-grids').querySelector(`.box-${box}[data-new='${id}']`).innerHTML:"";
+    return id!=""?document.querySelector('#original-grids-DK').querySelector(`.box-${box}[data-new='${id}']`).innerHTML:"";
 
 }
 // 1->1,4,7,10,13
 // 2->2,5,8,11,14
 // 3->3,6,9,12,15
-function createNewsFiltered(id1,id2,id3){
+function createRowNewsFilteredDK(id1,id2,id3){
 let ini1,ini2,ini3;
 ini1=(id1%3==0)?3:id1%3;
 ini2=(id2%3==0)?3:id2%3;
 ini3=(id3%3==0)?3:id3%3;
    let content=`
-   <div class="news__grid-DK" style="display:grid;">
+   <div class="news__grid-DK" style="display:none;">
     <div class="box-1">${getContent(id1,ini1)}</div> 
     <div class="box-2">${getContent(id2,ini2)}</div> 
     <div class="box-3">${getContent(id3,ini3)}</div> 
@@ -76,12 +99,102 @@ ini3=(id3%3==0)?3:id3%3;
     <div class="new-box__tags box-15">${getContent(id3,ini3+12)}</div>
     </div>
 `
-console.log("jaja")
-document.querySelector('#filtered-grids').innerHTML=content;
-document.querySelector('#original-grids').style.display="none";
+return content;
 }
 
+function createAllNewsFiltered(news){
+    rowsShow=0;
+    filtered=true;
+    let content=""
+    for(let i=0;i<news.length;i+=3){
+        content+=createRowNewsFilteredDK(news[i]||"",news[i+1]||"",news[i+2]||"");
+    }
 
+document.querySelector('#filtered-grids').innerHTML=content;
+document.querySelector('#original-grids-DK').style.display="none";
+showNexts();
+}
+
+function createRowNewsFilteredMB(id){
+
+    let content = `
+    <div class="news__grid-MB__new">
+    <div class="news__grid-MB__img">
+    ${getContent(id,ini1)}
+    </div>
+    <div class="news__grid-MB__info">
+        <div class="news__grid-MB__date">
+            <span>${getContent(id,ini1)}</span>
+        </div>
+        <div class="news__grid-MB__title">${getContent(id,ini1)}</div>
+    </div>
+</div>`
+}
+function generateOptionsFilter(){
+    let tags=[],content="";
+    document.querySelector('#original-grids-DK').querySelectorAll('.new-box__tag').forEach(node => {
+        tags.push(node.innerHTML);
+        
+     });
+     tags = [...new Set(tags)];
+     tags.forEach((tag,index)=>{
+        content+= `
+        <input id="checkbox${index}" type="checkbox"/>
+        <label for="checkbox${index}">${tag}</label>
+      
+        `
+     });
+     document.querySelector('#checkboxes').innerHTML=content;
+
+}
+
+function toggleCheckboxes() {
+    if (!expanded) 
+        openCheckBoxes();
+    else
+        closeCheckboxes();
+}
+
+function closeCheckboxes(){
+    
+    var checkboxes = document.getElementById("checkboxes");
+    checkboxes.style.display = "none";
+    expanded = false;
+    document.querySelector('#select').style.borderBottom="1px solid #707070";
+}
+
+function openCheckBoxes(){
+    
+    var checkboxes = document.getElementById("checkboxes");
+    checkboxes.style.display = "block";
+    expanded = true;
+    document.querySelector('#select').style.borderBottom="none";
+    
+}
+
+function applyFilter(){
+    let tags = [],news=[],allTags=[];
+    document.querySelector('#checkboxes').querySelectorAll('input').forEach(node=>{
+        if(node.checked)
+            tags.push(node.nextElementSibling.innerHTML);
+        allTags.push(node.nextElementSibling.innerHTML);
+    })
+  
+    if (tags.length==0)
+        tags=allTags;
+
+    news=searchNewsbyTag(tags);
+    createAllNewsFiltered(news);
+
+}
+
+window.addEventListener('click', function(e){   
+    if (!document.querySelector('.multiselect').contains(e.target)){
+       closeCheckboxes();
+    }
+  });
 showNexts();
 generateRandomTags();
-searchNewsbyTag();
+generateOptionsFilter();
+ 
+
