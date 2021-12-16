39e6b93e61b751b7188e53de5189a987a6d0f0c5
frmv <frmv@gft.com>
2021-12-16 11:23:07 +0100

    mejoras

--- participantes/scripts.js
+++ participantes/scripts.js
@@ -6,16 +6,21 @@ function toggleMostrar(id){
     if (node.dataset.state=="open"){
 
         node.querySelector('.equipo__pilotos').style.display="none";
+
+        if (document.documentElement.clientWidth > 768){
         node.querySelector('.equipo__mostrar-texto').style.display="block";
         node.querySelector('.equipo__ocultar-texto').style.display="none";
-
+        }
         node.querySelector('.equipo__mostrar-arrow').style.transform = "rotate(90deg)";
         node.dataset.state="close";
     }
     else{
         node.querySelector('.equipo__pilotos').style.display="grid";
+        
+        if (document.documentElement.clientWidth > 768){
         node.querySelector('.equipo__mostrar-texto').style.display="none";
         node.querySelector('.equipo__ocultar-texto').style.display="block";
+        }
         node.querySelector('.equipo__mostrar-arrow').style.transform = "rotate(270deg)";
         node.dataset.state="open";
     }
